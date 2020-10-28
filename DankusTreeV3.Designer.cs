1f18485d65cc22c140217ddc86d5123cc6e0b5e2
Oscar Davies <oscar.william.davies@outlook.com>
2020-10-28 09:14:55 +0000

    Add Tree Lights onto Tree

--- DankusTreeV3.Designer.cs
+++ DankusTreeV3.Designer.cs
@@ -30,13 +30,27 @@
         {
             this.components = new System.ComponentModel.Container();
             this.tmr_Snow = new System.Windows.Forms.Timer(this.components);
-            this.treeLight7 = new DankusTreeV3.TreeLight();
-            this.treeLight6 = new DankusTreeV3.TreeLight();
-            this.treeLight5 = new DankusTreeV3.TreeLight();
-            this.treeLight4 = new DankusTreeV3.TreeLight();
-            this.treeLight3 = new DankusTreeV3.TreeLight();
-            this.treeLight2 = new DankusTreeV3.TreeLight();
             this.treeLight1 = new DankusTreeV3.TreeLight();
+            this.treeLight2 = new DankusTreeV3.TreeLight();
+            this.treeLight3 = new DankusTreeV3.TreeLight();
+            this.treeLight4 = new DankusTreeV3.TreeLight();
+            this.treeLight5 = new DankusTreeV3.TreeLight();
+            this.treeLight6 = new DankusTreeV3.TreeLight();
+            this.treeLight7 = new DankusTreeV3.TreeLight();
+            this.treeLight8 = new DankusTreeV3.TreeLight();
+            this.treeLight9 = new DankusTreeV3.TreeLight();
+            this.treeLight10 = new DankusTreeV3.TreeLight();
+            this.treeLight11 = new DankusTreeV3.TreeLight();
+            this.treeLight12 = new DankusTreeV3.TreeLight();
+            this.treeLight13 = new DankusTreeV3.TreeLight();
+            this.treeLight14 = new DankusTreeV3.TreeLight();
+            this.treeLight15 = new DankusTreeV3.TreeLight();
+            this.treeLight16 = new DankusTreeV3.TreeLight();
+            this.treeLight17 = new DankusTreeV3.TreeLight();
+            this.treeLight18 = new DankusTreeV3.TreeLight();
+            this.treeLight19 = new DankusTreeV3.TreeLight();
+            this.treeLight20 = new DankusTreeV3.TreeLight();
+            this.treeLight21 = new DankusTreeV3.TreeLight();
             this.SuspendLayout();
             // 
             // tmr_Snow
@@ -45,68 +59,194 @@
             this.tmr_Snow.Interval = 30;
             this.tmr_Snow.Tick += new System.EventHandler(this.tmr_Snow_Tick);
             // 
-            // treeLight7
+            // treeLight1
             // 
-            this.treeLight7.BackColor = System.Drawing.Color.Red;
-            this.treeLight7.Location = new System.Drawing.Point(381, 429);
-            this.treeLight7.Name = "treeLight7";
-            this.treeLight7.Size = new System.Drawing.Size(5, 5);
-            this.treeLight7.TabIndex = 6;
+            this.treeLight1.BackColor = System.Drawing.Color.Red;
+            this.treeLight1.Location = new System.Drawing.Point(228, 419);
+            this.treeLight1.Name = "treeLight1";
+            this.treeLight1.Size = new System.Drawing.Size(7, 7);
+            this.treeLight1.TabIndex = 8;
+            // 
+            // treeLight2
+            // 
+            this.treeLight2.BackColor = System.Drawing.Color.White;
+            this.treeLight2.Location = new System.Drawing.Point(257, 431);
+            this.treeLight2.Name = "treeLight2";
+            this.treeLight2.Size = new System.Drawing.Size(7, 7);
+            this.treeLight2.TabIndex = 9;
+            // 
+            // treeLight3
+            // 
+            this.treeLight3.BackColor = System.Drawing.Color.Red;
+            this.treeLight3.Location = new System.Drawing.Point(289, 443);
+            this.treeLight3.Name = "treeLight3";
+            this.treeLight3.Size = new System.Drawing.Size(7, 7);
+            this.treeLight3.TabIndex = 10;
+            // 
+            // treeLight4
+            // 
+            this.treeLight4.BackColor = System.Drawing.Color.Red;
+            this.treeLight4.Location = new System.Drawing.Point(331, 455);
+            this.treeLight4.Name = "treeLight4";
+            this.treeLight4.Size = new System.Drawing.Size(7, 7);
+            this.treeLight4.TabIndex = 11;
+            // 
+            // treeLight5
+            // 
+            this.treeLight5.BackColor = System.Drawing.Color.Blue;
+            this.treeLight5.Location = new System.Drawing.Point(377, 455);
+            this.treeLight5.Name = "treeLight5";
+            this.treeLight5.Size = new System.Drawing.Size(7, 7);
+            this.treeLight5.TabIndex = 12;
             // 
             // treeLight6
             // 
             this.treeLight6.BackColor = System.Drawing.Color.Green;
-            this.treeLight6.Location = new System.Drawing.Point(352, 333);
+            this.treeLight6.Location = new System.Drawing.Point(435, 443);
             this.treeLight6.Name = "treeLight6";
-            this.treeLight6.Size = new System.Drawing.Size(5, 5);
-            this.treeLight6.TabIndex = 5;
+            this.treeLight6.Size = new System.Drawing.Size(7, 7);
+            this.treeLight6.TabIndex = 13;
             // 
-            // treeLight5
+            // treeLight7
             // 
-            this.treeLight5.BackColor = System.Drawing.Color.Green;
-            this.treeLight5.Location = new System.Drawing.Point(270, 243);
-            this.treeLight5.Name = "treeLight5";
-            this.treeLight5.Size = new System.Drawing.Size(5, 5);
-            this.treeLight5.TabIndex = 4;
+            this.treeLight7.BackColor = System.Drawing.Color.White;
+            this.treeLight7.Location = new System.Drawing.Point(476, 419);
+            this.treeLight7.Name = "treeLight7";
+            this.treeLight7.Size = new System.Drawing.Size(7, 7);
+            this.treeLight7.TabIndex = 14;
             // 
-            // treeLight4
+            // treeLight8
             // 
-            this.treeLight4.BackColor = System.Drawing.Color.Green;
-            this.treeLight4.Location = new System.Drawing.Point(293, 317);
-            this.treeLight4.Name = "treeLight4";
-            this.treeLight4.Size = new System.Drawing.Size(5, 5);
-            this.treeLight4.TabIndex = 3;
+            this.treeLight8.BackColor = System.Drawing.Color.Blue;
+            this.treeLight8.Location = new System.Drawing.Point(228, 358);
+            this.treeLight8.Name = "treeLight8";
+            this.treeLight8.Size = new System.Drawing.Size(7, 7);
+            this.treeLight8.TabIndex = 15;
             // 
-            // treeLight3
+            // treeLight9
             // 
-            this.treeLight3.BackColor = System.Drawing.Color.Green;
-            this.treeLight3.Location = new System.Drawing.Point(293, 387);
-            this.treeLight3.Name = "treeLight3";
-            this.treeLight3.Size = new System.Drawing.Size(5, 5);
-            this.treeLight3.TabIndex = 2;
+            this.treeLight9.BackColor = System.Drawing.Color.Red;
+            this.treeLight9.Location = new System.Drawing.Point(257, 369);
+            this.treeLight9.Name = "treeLight9";
+            this.treeLight9.Size = new System.Drawing.Size(7, 7);
+            this.treeLight9.TabIndex = 16;
             // 
-            // treeLight2
+            // treeLight10
             // 
-            this.treeLight2.BackColor = System.Drawing.Color.Red;
-            this.treeLight2.Location = new System.Drawing.Point(259, 356);
-            this.treeLight2.Name = "treeLight2";
-            this.treeLight2.Size = new System.Drawing.Size(5, 5);
-            this.treeLight2.TabIndex = 1;
+            this.treeLight10.BackColor = System.Drawing.Color.White;
+            this.treeLight10.Location = new System.Drawing.Point(307, 368);
+            this.treeLight10.Name = "treeLight10";
+            this.treeLight10.Size = new System.Drawing.Size(7, 7);
+            this.treeLight10.TabIndex = 17;
             // 
-            // treeLight1
+            // treeLight11
             // 
-            this.treeLight1.BackColor = System.Drawing.Color.White;
-            this.treeLight1.Location = new System.Drawing.Point(235, 430);
-            this.treeLight1.Name = "treeLight1";
-            this.treeLight1.Size = new System.Drawing.Size(5, 5);
-            this.treeLight1.TabIndex = 0;
+            this.treeLight11.BackColor = System.Drawing.Color.White;
+            this.treeLight11.Location = new System.Drawing.Point(352, 357);
+            this.treeLight11.Name = "treeLight11";
+            this.treeLight11.Size = new System.Drawing.Size(7, 7);
+            this.treeLight11.TabIndex = 18;
+            // 
+            // treeLight12
+            // 
+            this.treeLight12.BackColor = System.Drawing.Color.Blue;
+            this.treeLight12.Location = new System.Drawing.Point(377, 335);
+            this.treeLight12.Name = "treeLight12";
+            this.treeLight12.Size = new System.Drawing.Size(7, 7);
+            this.treeLight12.TabIndex = 19;
+            // 
+            // treeLight13
+            // 
+            this.treeLight13.BackColor = System.Drawing.Color.Blue;
+            this.treeLight13.Location = new System.Drawing.Point(414, 312);
+            this.treeLight13.Name = "treeLight13";
+            this.treeLight13.Size = new System.Drawing.Size(7, 7);
+            this.treeLight13.TabIndex = 20;
+            // 
+            // treeLight14
+            // 
+            this.treeLight14.BackColor = System.Drawing.Color.Red;
+            this.treeLight14.Location = new System.Drawing.Point(288, 262);
+            this.treeLight14.Name = "treeLight14";
+            this.treeLight14.Size = new System.Drawing.Size(7, 7);
+            this.treeLight14.TabIndex = 21;
+            // 
+            // treeLight15
+            // 
+            this.treeLight15.BackColor = System.Drawing.Color.Red;
+            this.treeLight15.Location = new System.Drawing.Point(330, 279);
+            this.treeLight15.Name = "treeLight15";
+            this.treeLight15.Size = new System.Drawing.Size(7, 7);
+            this.treeLight15.TabIndex = 22;
+            // 
+            // treeLight16
+            // 
+            this.treeLight16.BackColor = System.Drawing.Color.Red;
+            this.treeLight16.Location = new System.Drawing.Point(376, 288);
+            this.treeLight16.Name = "treeLight16";
+            this.treeLight16.Size = new System.Drawing.Size(7, 7);
+            this.treeLight16.TabIndex = 23;
+            // 
+            // treeLight17
+            // 
+            this.treeLight17.BackColor = System.Drawing.Color.Green;
+            this.treeLight17.Location = new System.Drawing.Point(413, 262);
+            this.treeLight17.Name = "treeLight17";
+            this.treeLight17.Size = new System.Drawing.Size(7, 7);
+            this.treeLight17.TabIndex = 24;
+            // 
+            // treeLight18
+            // 
+            this.treeLight18.BackColor = System.Drawing.Color.Green;
+            this.treeLight18.Location = new System.Drawing.Point(289, 199);
+            this.treeLight18.Name = "treeLight18";
+            this.treeLight18.Size = new System.Drawing.Size(7, 7);
+            this.treeLight18.TabIndex = 25;
+            // 
+            // treeLight19
+            // 
+            this.treeLight19.BackColor = System.Drawing.Color.Blue;
+            this.treeLight19.Location = new System.Drawing.Point(329, 217);
+            this.treeLight19.Name = "treeLight19";
+            this.treeLight19.Size = new System.Drawing.Size(7, 7);
+            this.treeLight19.TabIndex = 26;
+            // 
+            // treeLight20
+            // 
+            this.treeLight20.BackColor = System.Drawing.Color.Blue;
+            this.treeLight20.Location = new System.Drawing.Point(375, 231);
+            this.treeLight20.Name = "treeLight20";
+            this.treeLight20.Size = new System.Drawing.Size(7, 7);
+            this.treeLight20.TabIndex = 27;
+            // 
+            // treeLight21
+            // 
+            this.treeLight21.BackColor = System.Drawing.Color.Green;
+            this.treeLight21.Location = new System.Drawing.Point(413, 230);
+            this.treeLight21.Name = "treeLight21";
+            this.treeLight21.Size = new System.Drawing.Size(7, 7);
+            this.treeLight21.TabIndex = 28;
             // 
             // DankusTree
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(682, 620);
+            this.ClientSize = new System.Drawing.Size(684, 611);
+            this.Controls.Add(this.treeLight21);
+            this.Controls.Add(this.treeLight20);
+            this.Controls.Add(this.treeLight19);
+            this.Controls.Add(this.treeLight18);
+            this.Controls.Add(this.treeLight17);
+            this.Controls.Add(this.treeLight16);
+            this.Controls.Add(this.treeLight15);
+            this.Controls.Add(this.treeLight14);
+            this.Controls.Add(this.treeLight13);
+            this.Controls.Add(this.treeLight12);
+            this.Controls.Add(this.treeLight11);
+            this.Controls.Add(this.treeLight10);
+            this.Controls.Add(this.treeLight9);
+            this.Controls.Add(this.treeLight8);
             this.Controls.Add(this.treeLight7);
             this.Controls.Add(this.treeLight6);
             this.Controls.Add(this.treeLight5);
@@ -131,6 +271,20 @@
         private TreeLight treeLight5;
         private TreeLight treeLight6;
         private TreeLight treeLight7;
+        private TreeLight treeLight8;
+        private TreeLight treeLight9;
+        private TreeLight treeLight10;
+        private TreeLight treeLight11;
+        private TreeLight treeLight12;
+        private TreeLight treeLight13;
+        private TreeLight treeLight14;
+        private TreeLight treeLight15;
+        private TreeLight treeLight16;
+        private TreeLight treeLight17;
+        private TreeLight treeLight18;
+        private TreeLight treeLight19;
+        private TreeLight treeLight20;
+        private TreeLight treeLight21;
     }
 }
 
--- TreeLight.Designer.cs
+++ TreeLight.Designer.cs
@@ -42,7 +42,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Name = "TreeLight";
-            this.Size = new System.Drawing.Size(5, 5);
+            this.Size = new System.Drawing.Size(10, 10);
             this.Load += new System.EventHandler(this.TreeLight_Load);
             this.ResumeLayout(false);
 
